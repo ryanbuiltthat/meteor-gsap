fe4417848c08e02bd94bec4a68d260c4ded76696
Brent Anderson <me@brentjanderson.com>
2015-03-12 15:28:27 -0600

    Update to package.js to reflect 1.16.0 package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'GreenSock Animation Platform : Professional-Grade HTML5 Animation.',
-  version: "1.14.2",
+  version: "1.16.0",
   git: "https://github.com/infinitedg/meteor-gsap/"
 });
 
